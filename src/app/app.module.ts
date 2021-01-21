1bf3c1f2bc580de6991873ac9f06f6887d76be89
Matheus Cristian <mathues@email.com>
2021-01-21 11:07:46 -0300

    Adds Button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { PlayerSelectorComponent } from './components/player-selector/player-selector.component';
 import { TicTacToeComponent } from './components/tic-tac-toe/tic-tac-toe.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerSelectorComponent,
     TicTacToeComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
